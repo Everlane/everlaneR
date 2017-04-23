97b011b1985812c17fd9cba10a22dfd31fc43f3d
jasonjea <jasonzjea@gmail.com>
2017-04-22 23:12:14 -0700

    comments

--- R/devops.R
+++ R/devops.R
@@ -1,42 +1,73 @@
-get_aws_credentials <- function() {
-  aws_creds_file = file(paste0(Sys.getenv("HOME"),'/.aws/','credentials'), "r")
-  aws_creds <- list()
-  while (TRUE) {
-    line = readLines(aws_creds_file, n = 1, warn = FALSE)
-    if ( length(line) == 0 ) {
-      break
+# credential and connection management for everlane data
+#
+# basic tasks:
+# - store and manage aws credentials
+# - store and manage redshift credentials
+# - connect to redshift db
+#
+
+# store aws credentials- necessary for interacting with S3 and data pipelines
+#############################################################################
+  get_aws_credentials <- function() {
+    # reads keys from credentials file in the ~/.aws/ folder (requires user to have aws cli installed)
+    aws_creds_file = file(paste0(Sys.getenv("HOME"),'/.aws/','credentials'), "r")
+
+    # initialize empty list to store aws credentials
+    aws_creds <- list()
+
+    # read in list of credentials
+    while (TRUE) {
+      # loop through lines in credentials files
+      line = readLines(aws_creds_file, n = 1, warn = FALSE)
+      if ( length(line) == 0 ) {
+        break # break loop if line is empty
+      }
+      line <- gsub("\n", "", gsub(" ", "", line)) # basic scrubbing of newline character and empty spaces
+
+      # create key-value element in list for credential in current line
+      # key is character before '=', value is character after '='
+      aws_creds[[substr(line, 1, regexpr('=', line)[1]- 1)]] <- substr(line, regexpr('=', line)[1] + 1, nchar(line))
     }
-    line <- gsub("\n", "", gsub(" ", "", line))
-    aws_creds[[substr(line, 1, regexpr('=', line)[1]- 1)]] <- substr(line, regexpr('=', line)[1] + 1, nchar(line))
+    close(aws_creds_file)
+    return(aws_creds)
   }
-  close(aws_creds_file)
-  return(aws_creds)
-}
 
-get_redshift_credentials <- function() {
-  aws_creds_file = file(paste0(Sys.getenv("HOME"),'/.aws/','redshift'), "r")
-  aws_creds <- list()
-  while (TRUE) {
-    line = readLines(aws_creds_file, n = 1, warn = FALSE)
-    if ( length(line) == 0 ) {
-      break
+# store redshift credentials- necessary for querying redshift datastore
+#############################################################################
+  get_redshift_credentials <- function() {
+    # reads keys from redshift file in the ~/.aws/ folder (requires user to have aws cli installed)
+    redshift_creds_file = file(paste0(Sys.getenv("HOME"),'/.aws/','redshift'), "r")
+    redshift_creds <- list()
+
+    # loop through lines in credentials files
+    while (TRUE) {
+      line = readLines(redshift_creds_file, n = 1, warn = FALSE)
+      if ( length(line) == 0 ) {
+        break # break loop if line is empty
+      }
+      line <- gsub("\n", "", gsub(" ", "", line)) # basic scrubbing of newline character and empty spaces
+
+      # create key-value element in list for credential in current line
+      # key is character before '=', value is character after '='
+      redshift_creds[[substr(line, 1, regexpr('=', line)[1]- 1)]] <- substr(line, regexpr('=', line)[1] + 1, nchar(line))
     }
-    line <- gsub("\n", "", gsub(" ", "", line))
-    aws_creds[[substr(line, 1, regexpr('=', line)[1]- 1)]] <- substr(line, regexpr('=', line)[1] + 1, nchar(line))
+    close(redshift_creds)
+    return(redshift_creds)
   }
-  close(aws_creds_file)
-  return(aws_creds)
-}
 
-create_redshift_con <- function() {
-  require(RPostgreSQL)
-  drv <- dbDriver("PostgreSQL")
-  connection_details <- get_redshift_credentials()
-  return(
-    dbConnect(drv, host = connection_details['host'],
-              dbname = connection_details['dbname'],
-              user = connection_details['user'], password = connection_details['password'],
-              port = connection_details['port'])
-  )
+# construct redshift connection string and connect to redshift db
+#############################################################################
+  create_redshift_con <- function() {
+    require(RPostgreSQL)
+    drv <- dbDriver("PostgreSQL")
+    connection_details <- get_redshift_credentials() # create list containing redshift credentials
 
-}
+    # connect to redshift using the dbConnect function from the RPostgreSQL package
+    return(
+      dbConnect(drv, host = connection_details['host'],
+                dbname = connection_details['dbname'],
+                user = connection_details['user'], password = connection_details['password'],
+                port = connection_details['port'])
+    )
+
+  }
--- R/package_dependencies.R
+++ R/package_dependencies.R
@@ -1,20 +1,17 @@
-# Hello, world!
+# easy package dependency management for deploying code to remote servers
 #
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
+# basic idea is to have a package installation playbook that installs common
+# packages to a temp folder on the cloud server.
 #
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
 
+# function for quickly checking if package is already installed
+############################################################
 install_packages_if <- function(package_name, lib_path) {
+  # check if package is installed
   if ( !package_name %in% rownames(installed.packages()) ) {
+
+    # optional arugment lib_path- can specify specific libpath to install to
+    # if missing, install to default dir
     if ( missing(lib_path) ) {
       install.packages(package_name)
     }
@@ -23,24 +20,31 @@ install_packages_if <- function(package_name, lib_path) {
                        repos = "http://cran.us.r-project.org")
     }
   }
-  else {
+  else { # if package is already installed, return message
     message (paste0(package_name, " is already installed!"))
   }
 }
 
+# package installation playbook
+############################################################
 package_dependecy_playbook <- function(additional_packages) {
-  .libPaths(c(.libPaths(), temp <- tempdir()))
+  .libPaths(c(.libPaths(), temp <- tempdir())) # add temp directory to libpaths (R looks at these paths when loading packages)
+
+  # vector of commonly used packages
   packages <- c("dply", "plyr", "reshape2" , "ggplot2", "RPostgreSQL", "lubridate",
                 "scales", "tidyr", "zoo", "lazyeval", "devtools")
 
+  # optional argument of manually appending additional packages to install
   if (!missing(additional_packages)) {
     packages <- c(packages, additional_packages)
   }
 
+  # install packages to temp dir
   for (package in packages) {
     install_packages_if(package_name = package, lib_path = temp)
   }
 
+  # install packages from github
   require(devtools)
   withr::with_libpaths(new = temp, install_github("cloudyr/aws.s3"))
 }
