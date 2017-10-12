db608d3f47e7f15427d3122d4e4076724550ecbe
IVEE <iveecojuangco@gmail.com>
2017-10-12 12:17:26 -0700

    removing working directory pathway lines, user must setwd() before running this function

--- R/upload_to_s3.R
+++ R/upload_to_s3.R
@@ -1,13 +1,9 @@
-# Purpose: To easily upload files to S3. Objects to call within function are working directory path, file name and objectname. File and object name are usually the same input but in the event that the user wants them to be different, both inputs must be made in the function.
-upload_to_s3 <- function(directory_path, file_name, object_name) {
+# Purpose: To easily upload files to S3. Objects to call within function are working file name and object name. File and object name are usually the same input but in the event that the user wants them to be different, both inputs must be made in the function.
+# Note: You must set your working directory before you run this function. File and object name must have entire pathway name
+upload_to_s3 <- function(file_name, object_name) {
 
 # Install package dependency
   require(aws.s3)
-
-# Set up working directory path
-  message(paste0("working dir:", getwd()))
-  setwd(paste0(getwd(), directory_path))
-  message(paste0("working dir:", getwd()))
 
 # Read credentials from config
   aws_creds <- get_aws_credentials()
