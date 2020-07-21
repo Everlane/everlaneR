19380d55aea3d89619f953deb341e564f9f3ff96
Gisela Morrone <gisela.morrone@everlane.com>
2020-07-21 11:46:22 -0300

    Fix

--- R/redshift_spectrum.R
+++ R/redshift_spectrum.R
@@ -25,38 +25,41 @@
 #'  condition = "select distinct insert_date from external_marketing.browse_recommendations_filtered_x_days where date >= '2019-03-13' - 7",
 #'  partition_field = "insert_date",
 #'  table = "external_marketing.browse_recommendations_filtered_x_days",
-#'  bucket = "everlane-data-athena-marketing"
+#'  bucket_name = "everlane-data-athena-marketing"
 #'  s3_key = "browse_recommendations_filtered_x_days/"
 #' )
 #'
-delete_partition_external_table <- function(redshift, condition, partition_field, table, bucket, s3_key) {
+
+delete_partition_external_table <- function(redshift, condition, partition_field, table, bucket_name, s3_key) {
   # Get partitions to delete (Run "condition" sql that returns which partitions to delete)
   partitions_to_delete <- dbGetQuery(redshift, condition)
-
-  for (row in 1:nrow(partitions_to_delete)){
-    # Delete partitions from table (Run alter drop partition)
-    query =
-      sprintf(
-        "
+  print(partitions_to_delete)
+  if (nrow(partitions_to_delete) != 0 ){
+    for (row in 1:nrow(partitions_to_delete)){
+      # Delete partitions from table (Run alter drop partition)
+      query =
+        sprintf(
+          "
             alter table %s
             drop partition (%s = '%s');
             ",
-        table,
-        partition_field,
-        partitions_to_delete[row, partition_field]
-      )
-    dbGetQuery(redshift, query)
-    message("Partition ", partitions_to_delete[row, partition_field], " deleted")
+          table,
+          partition_field,
+          partitions_to_delete[row, partition_field]
+        )
+      dbGetQuery(redshift, query)
+      message("Partition ", partitions_to_delete[row, partition_field], " deleted")
 
-    # Delete corresponding S3 keys from bucket
-    # Bucket contents in dataframe
-    s3_prefix <- sprintf("%s%s=%s", s3_key, partition_field, partitions_to_delete[row, partition_field])
+      # Delete corresponding S3 keys from bucket
+      # Bucket contents in dataframe
+      s3_prefix <- sprintf("%s%s=%s", s3_key, partition_field, partitions_to_delete[row, partition_field])
 
-    bucket <- get_bucket_df(bucket = bucket_name, prefix = s3_prefix)
-    # Loop through file list
-    for(file in bucket$Key){
-      delete_object(file, bucket = bucket_name, prefix = s3_prefix)
-      message("File ", file, " deleted from s3")
+      bucket <- get_bucket_df(bucket = bucket_name, prefix = s3_prefix)
+      # Loop through file list
+      for(file in bucket$Key){
+        delete_object(file, bucket = bucket_name, prefix = s3_prefix)
+        message("File ", file, " deleted from s3")
+      }
     }
   }
 }
